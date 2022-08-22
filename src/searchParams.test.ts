ef9ef3c956b7585e12eefec196c18c25c44e7a39
Eduardo Vicente <eduardomcv42@gmail.com>
2022-08-22 20:09:52 +0100

    Add tests for generator functions

---
+++ src/searchParams.test.ts
@@ -0,0 +1,33 @@
+import { generateDictionaryFromSearchString, generateSearchStringFromDictionary, SearchParamsDictionary } from './searchParams';
+
+describe('generateDictionaryFromSearchString', () => {
+  it('should generate a dictionary structure from a provided search string', () => {
+    const searchString = 'param1=test&param2=test1&param2=test2';
+    const dictionary = generateDictionaryFromSearchString(searchString);
+
+    expect(dictionary).toStrictEqual(
+      {
+        param1: 'test',
+        param2: [
+          'test1',
+          'test2',
+        ],
+      },
+    );
+  });
+});
+
+describe('generateSearchStringFromDictionary', () => {
+  it('should generate a valid search string from a provided dictionary', () => {
+    const dictionary: SearchParamsDictionary = {
+      param1: 'test',
+      param2: [
+        'test1',
+        'test2',
+      ],
+    };
+
+    const searchString = generateSearchStringFromDictionary(dictionary);
+    expect(searchString).toBe('param1=test&param2=test1&param2=test2');
+  });
+});
