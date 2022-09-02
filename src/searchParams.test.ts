36de1a73ac8268a929f2c01bb37e2d4a82c50643
Eduardo Vicente <eduardomcv42@gmail.com>
2022-09-02 15:58:47 +0100

    Add test for getSearchParamsDictionary

--- src/searchParams.test.ts
+++ src/searchParams.test.ts
@@ -1,4 +1,9 @@
-import { generateDictionaryFromSearchString, generateSearchStringFromDictionary, SearchParamsDictionary } from './searchParams';
+import {
+  generateDictionaryFromSearchString,
+  generateSearchStringFromDictionary,
+  getSearchParamsDictionary,
+  SearchParamsDictionary,
+} from './searchParams';
 
 describe('generateDictionaryFromSearchString', () => {
   it('should generate a dictionary structure from a provided search string', () => {
@@ -29,5 +34,37 @@ describe('generateSearchStringFromDictionary', () => {
 
     const searchString = generateSearchStringFromDictionary(dictionary);
     expect(searchString).toBe('param1=test&param2=test1&param2=test2');
+  });
+});
+
+describe('getSearchParamsDictionary', () => {
+  let windowSpy: jest.SpyInstance;
+
+  beforeEach(() => {
+    windowSpy = jest.spyOn(window, 'window', 'get');
+  });
+
+  afterEach(() => {
+    windowSpy.mockRestore();
+  });
+
+  it('should retrieve a dictionary from the search params', () => {
+    windowSpy.mockImplementation(() => ({
+      location: {
+        search: 'param1=test&param2=test1&param2=test2',
+      },
+    }));
+
+    const dictionary = getSearchParamsDictionary();
+
+    expect(dictionary).toStrictEqual(
+      {
+        param1: 'test',
+        param2: [
+          'test1',
+          'test2',
+        ],
+      },
+    );
   });
 });
